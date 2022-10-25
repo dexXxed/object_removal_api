02918cce5aa9a4e087e0acc55dc2405611809278
dexxxed <dexxxed.app@gmail.com>
2022-10-25 13:33:58 +0300

    GPU support

--- src/core.py
+++ src/core.py
@@ -45,9 +45,9 @@ ENERGY_MASK_CONST = 100000.0  # large energy value for protective masking
 MASK_THRESHOLD = 10  # minimum pixel intensity for binary mask
 USE_FORWARD_ENERGY = True  # if True, use forward energy algorithm
 
-device = torch.device("cpu")
+device = torch.device("gpu")
 model_path = "./assets/big-lama.pt"
-model = torch.jit.load(model_path, map_location="cpu")
+model = torch.jit.load(model_path, map_location="gpu")
 model = model.to(device)
 model.eval()
 
